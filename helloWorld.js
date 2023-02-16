4262c904f1a323d3eb1202ac33b05b6ec479d375
ajinkyadombe310 <ajinkya.dombe310@gmail.com>
2023-02-16 22:04:47 +0530

    hello world program
    hello world program

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
 console.log("Mohit Sharma");
+console.log("32")
+console.log("Name")
+var fullName = "Mohit Sharma";
+console.log(fullName);
+var city;
+console.log(city);
+city="Pune";
+console.log(city);
+city="Surat";
+console.log(city);

c6719bcb53ad51ecfc624ecf6a684fce103e64c6
ajinkyadombe310 <ajinkya.dombe310@gmail.com>
2023-02-17 22:24:40 +0530

    swap variable practice
    swap variable practice

---
+++ 01_swapVarassign.js
@@ -0,0 +1,32 @@
+console.log("********Swaping variable value******");
+console.log("Step:1");
+var namesSweety = "Sweety";
+var nameCutie = "Cutie";
+var temp = "Sweety"
+namesSweety=nameCutie;
+nameCutie=temp;
+console.log(namesSweety, nameCutie);
+console.log("Step:2");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+var temp1 = 100;
+var temp2 = 200;
+var temp3 = 300;
+console.log(num1, num2, num3);
+num1 = num2;
+num2 = temp1;
+console.log(num1, num2);
+num2 = num3;
+num3 = temp2;
+console.log(num2, num3);
+num3 = temp1;
+num1 = temp3;
+console.log(num3, num1);
+
+
+
+
+
+
+

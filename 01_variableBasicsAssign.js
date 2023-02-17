b42ec0531ae723f926c25c2a91d26b6299fe8eee
ajinkyadombe310 <ajinkya.dombe310@gmail.com>
2023-02-17 22:25:00 +0530

    variable basic practice
    variable basic practice

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,14 @@
+console.log("*****Basic of Variable*****");
+console.log("Step:1");
+var pin = 451248;
+var tname = "Mohit";
+var isMarried = true;
+console.log("PIN:", pin, ",", "Type:", typeof pin);
+console.log("NAME:", tname, ",", "Type:", typeof tname);
+console.log("Is Married?:", isMarried, ",", "Type:", typeof isMarried);
+console.log("Step:2");
+var basicDetails = null;
+console.log("value of var:", basicDetails, ",", typeof basicDetails);
+var same;
+console.log("value of same var:", same);
+

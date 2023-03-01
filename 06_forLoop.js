1817e63fc68d7198c24987f58410e00783dda907
ajinkyadombe310 <ajinkya.dombe310@gmail.com>
2023-03-01 21:46:15 +0530

    for loop assign
    for loop assign

---
+++ 06_forLoop.js
@@ -0,0 +1,30 @@
+for (let index = 5; index<=15; index=index+1) {
+    console.log(index);
+    
+}
+console.log("***************");
+
+for (let index = 50; index >=40; index=index-1) {
+    console.log(index);
+    
+}
+console.log("***************");
+for (let index = 1; index <=15 ; index=index+2) {
+    console.log(index);
+        
+}
+console.log("***************");
+for (let index = 2; index <=10 ; index=index+2) {
+    console.log(index);
+        
+}
+console.log("***************");
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);
+        
+}
+console.log("***************");
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+        
+}

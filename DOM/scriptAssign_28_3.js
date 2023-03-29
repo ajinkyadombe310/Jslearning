85265ff810a8431ab5bf209871c170b123a468fe
ajinkyadombe310 <ajinkya.dombe310@gmail.com>
2023-03-29 06:37:07 +0530

    DOM Manipulation assign_27_03_23
    DOM Manipulation assign_27_03_23

---
+++ DOM/scriptAssign_28_3.js
@@ -0,0 +1,7 @@
+const mainHeading = document.querySelector("#heading");
+console.log(mainHeading.innerHTML);
+const secondAdvantage = document.querySelector("#secondadvantage");
+console.log(secondAdvantage.innerHTML);
+const advDiv = document.querySelector("#advantages");
+console.log(advDiv.innerHTML);
+
